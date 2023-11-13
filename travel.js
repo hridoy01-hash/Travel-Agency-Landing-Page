2cdb80f4da2b8fd7c5079274c413240cbc23111c
hridoye <hridoysagor171@gmail.comm>
2023-11-13 15:37:28 +0600

    header complete with mobile responsive

--- travel.js
+++ travel.js
@@ -2,7 +2,11 @@ const Hamburger_id = document.getElementById("Hamburger_id");
 const side_bar_main_wrapper = document.querySelector(".side_bar_main_wrapper");
 const sidebar_inner_content_wrapper = document.querySelector(".sidebar_inner_content_wrapper");
 Hamburger_id.addEventListener("click", function () {
-    console.log("btn_clicked");
     side_bar_main_wrapper.classList.add("active_mobile_sidebar");
     sidebar_inner_content_wrapper.classList.add("open_sidebar");
+});
+const sidebar_hide_btn_ID = document.getElementById("sidebar_hide_btn_ID");
+sidebar_hide_btn_ID.addEventListener("click", function () {
+    side_bar_main_wrapper.classList.remove("active_mobile_sidebar");
+    sidebar_inner_content_wrapper.classList.remove("open_sidebar");
 });
